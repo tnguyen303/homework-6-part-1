9a68a46f54a60a642599bc4504ea4d3cb1fae515
tnguyen303 <tringuyen552911@gmail.com>
2018-10-28 22:18:29 -0400

    working delete route

--- public/app.js
+++ public/app.js
@@ -1,81 +1,45 @@
-const toDoList = ["Buy diaper", "Change light bulb", "Do homework", "Clean room", "Take wife out"];
 
-let index = 0;
-toDoList.forEach(e => {
-    $('#content').append(`
-    <div id='item-${index}' class="toDoItem">
-    <input class="finishedBtn" type="checkbox">
-    <span>${e}</span>
-    <a href="#"><span id='deleteBtn-${index}' class="delete fas fa-times" value='${index}'></span></a>
-    </div>`);
-    index ++;
-});
+const render = function(outputElement, dataList) {
+  let index = 0;
+  dataList.forEach(e => {
+    $(outputElement).append(`
+        <div id='item-${index}' class="toDoItem">
+        <input class="finishedBtn" type="checkbox">
+        <span>${e.task}</span>
+        <a href="#"><span id='deleteBtn-${
+          index
+        }' class="delete fas fa-times" value='${index}'></span></a>
+        </div>`);
+    index++;
+  });
+};
 
-$('.fa-share').on('click', function (event) {
-    event.preventDefault();
-    $('#content').append(`
-    <div id='item-${index}' class="toDoItem">
-    <input class="finishedBtn" type="checkbox">
-    <span>${$('#newInput').val().trim()}</span>
-    <a href="#"><span id='deleteBtn-${index}' class="delete fas fa-times" value='${index}'></span></a>
-    </div>`)
+//add a new todo item to list
+$(".fa-share").on("click", function(event) {
+  event.preventDefault();
+  // $('#content').html('');
+  const newInput = {task: $('#newInput').val().trim(), done: false};
+  const newInputList = [newInput];
+  $.ajax({ url: '/api/todolist/', method: 'POST', data: newInput}).then(function(list){
+    render("content", list);
     $('#newInput').val('');
-    toDoList.push($('#newInput').val().trim());
-    index ++;
+  });
 });
 
-$(document).ready(function(){
-    $(document).on('click', '.delete', function(event){
-        event.preventDefault();
-        //extract number from value property of clicked button
-        const deleteId = $(this).attr('value');
-        $(`#item-${deleteId}`).remove();
+//function to delete a todo item
+$(document).ready(function() {
+  $(document).on("click", ".delete", function(event) {
+    event.preventDefault();
+    //extract number from value property of clicked button
+    const deleteId = $(this).attr("value");
+    $(`#item-${deleteId}`).remove();
+    $.ajax({ url: `/api/todolist/${deleteId}`, method: "DELETE" });
+    $.ajax({ url: "/api/todolist/", method: "GET" }).then(function(data) {
+      render("content", data);
     });
+  });
 });
 
-// Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.post('/api/todolist', function (req, res) {
-//     console.log(req.body);
-//     //end response
-//     res.end();
-// });
-
-// app.delete('/', function (req, res) {
-//     toDoList[toDoList.findIndex(e => e.)]//not sure how this is handled
-//     res.end();
-// });
-
-// $.ajax({ url: '/api/todolist', method: 'POST', data: newReservation }).then(
-//     function (data) {
-
-//         // If our POST request was successfully processed, proceed on
-//         if (data.success) {
-
-//             console.log('data', data)
-//             // If a table is available... tell user they are booked.
-//             if (!data.waitlist) {
-//                 alert('Yay! You are officially booked!');
-//             }
-
-//             // If a table is available... tell user they on the waiting list.
-//             else {
-//                 alert('Sorry you are on the wait list');
-//             }
-
-//             // Clear the form when submitting
-//             $('#reserve-name').val('');
-//             $('#reserve-phone').val('');
-//             $('#reserve-email').val('');
-//             $('#reserve-unique-id').val('');
-
-//             $('#reserve-name').focus();
-//         } else {
-
-//             alert('There was a problem with your submission. Please check your entry and try again.');
-//         }
-
-
-//     });
+$.ajax({ url: "/api/todolist", method: "GET" }).then(function(data) {
+  render("#content", data);
+});
--- server.js
+++ server.js
@@ -1,32 +1,42 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const toDoList = [
+  {task: "Buy diaper", done: false},
+  {task: "Change light bulb", done: false},
+  {task: "Do homework", done: false},
+  {task: "Clean room", done: false},
+  {task: "Take wife out", done: false}
+];
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //give access to public files such as css and html files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //get index.html file
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index"));
 });
 
-app.post('/api/todolist', function (req, res) {
-    console.log(req.body);
-    //end response
-    res.end();
+app.get("/api/todolist", function(req, res) {
+  res.json(toDoList);
 });
 
-app.delete('/', function(req,res){
-    // toDoList[toDoList.findIndex(e => e.)]//not sure how this is handled
-    // res.end();
+app.post("/api/todolist", function(req, res) {
+  toDoList.push(req.body);
+  res.json(toDoList);
 });
 
-app.listen(PORT, function () {
-    console.log(`App listening on PORT ${PORT}`);
+app.delete("/api/todolist/:deleteID", function(req, res) {
+  toDoList.splice(req.params.deleteID, 1);
+  res.end();
 });
 
+app.listen(PORT, function() {
+  console.log(`App listening on PORT ${PORT}`);
+});
